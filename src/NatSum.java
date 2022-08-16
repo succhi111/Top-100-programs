c10353c40fac070690351ecfbf6c867c9cdfbb77
succhi111 <suchendramishra396@gmail.com>
2022-08-16 16:52:38 +0530

    program to find SUMof given natural numbers

---
+++ src/NatSum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class NatSum
+{
+    public static void main(String[] args) {
+        int n,sum=0;
+        System.out.println("Enter number of term");
+        Scanner r=new Scanner(System.in);
+        n=r.nextInt();
+        for(int i=1;i<=n;i++) {
+            sum=sum+i;
+        }
+        System.out.println("ADDITION=" + sum);
+        }
+
+    }
+
