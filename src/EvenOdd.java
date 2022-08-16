c1b7636d22b85455d33287898994e31821960f07
succhi111 <suchendramishra396@gmail.com>
2022-08-16 16:14:12 +0530

    program to find EVEN ODD number

---
+++ src/EvenOdd.java
@@ -0,0 +1,15 @@
+//program to check whether the number is even or odd
+public class EvenOdd
+{
+        public static void main(String[] args) {
+            int number = 13;
+
+
+
+            if (number % 2 == 0)
+                System.out.println(number + " is Even");
+            else
+                System.out.println(number + " is odd");
+        }
+    }
+
