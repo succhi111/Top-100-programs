79d87ebc9a9efeb76e678ab1495929a2b1bece9d
succhi111 <suchendramishra396@gmail.com>
2022-08-16 15:05:45 +0530

    Initial commit

---
+++ src/PosNeg.java
@@ -0,0 +1,14 @@
+//Program to check if the number is negative or positive
+
+public class PosNeg {
+    public static void main(String[] args) {
+        int num = -3;
+        if (num > 0)
+            System.out.println("The number is positive");
+        else if (num < 0)
+            System.out.println("The number is negative");
+            else
+            System.out.println("zero");
+
+    }
+}
